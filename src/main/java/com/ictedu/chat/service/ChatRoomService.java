8fe54a543d9b4c708f7a89eaa3c8b375211bb3ce
YOONJEUN <001.t3st@gmail.com>
2024-08-21 16:54:24 +0900

    feat: 채팅 Controller -ing date: 240821

--- src/main/java/com/ictedu/chat/service/ChatRoomService.java
+++ src/main/java/com/ictedu/chat/service/ChatRoomService.java
@@ -4,7 +4,12 @@ import org.springframework.stereotype.Service;
 
 import com.ictedu.chat.dto.ChatRoomDTO;
 import com.ictedu.chat.entity.ChatRoom;
+import com.ictedu.chat.entity.ChatRoomUsers;
 import com.ictedu.chat.repository.ChatRoomRepository;
+import com.ictedu.chat.repository.ChatRoomUsersRepository;
+import com.ictedu.user.model.entity.User;
+import com.ictedu.user.repository.UserRepository;
+
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -12,14 +17,28 @@ import lombok.RequiredArgsConstructor;
 public class ChatRoomService {
 
     private final ChatRoomRepository chatRoomRepository;
+    private final ChatRoomUsersRepository chatRoomUsersRepository;
+    private final UserRepository userRepository;
 
-    public ChatRoomDTO createChatRoom(ChatRoomDTO chatRoomDTO) {
+    public ChatRoomDTO createChatRoom(ChatRoomDTO chatRoomDTO, Long[] userIds) {
         // DTO -> Entity 변환
         ChatRoom chatRoom = ChatRoomDTO.toEntity(chatRoomDTO);
 
         // 데이터베이스에 저장
         ChatRoom savedChatRoom = chatRoomRepository.save(chatRoom);
 
+        
+        // 선택된 userId로 ChatRoomUsers 엔터티 생성 및 저장
+        for (Long userId : userIds) {
+            User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("Invalid user ID: " + userId));
+            ChatRoomUsers chatRoomUsers = new ChatRoomUsers();
+            chatRoomUsers.setUser(user);
+            chatRoomUsers.setChatroom(savedChatRoom);
+            chatRoomUsersRepository.save(chatRoomUsers);
+        }
+        
+        
+        
         // Entity -> DTO 변환 후 반환
         return ChatRoomDTO.toDto(savedChatRoom);
     }
