34f3eb9d5f3c9f90aec5380940f4b7ce8cab4774
YOONJEUN <001.t3st@gmail.com>
2024-08-23 18:25:35 +0900

    feat: error 주석 ; date: 240823

--- src/main/java/com/ictedu/chat/service/ChatRoomService.java
+++ src/main/java/com/ictedu/chat/service/ChatRoomService.java
@@ -20,26 +20,26 @@ public class ChatRoomService {
     private final ChatRoomUsersRepository chatRoomUsersRepository;
     private final UserRepository userRepository;
 
-    public ChatRoomDTO createChatRoom(ChatRoomDTO chatRoomDTO, Long[] userIds) {
-        // DTO -> Entity 변환
-        ChatRoom chatRoom = ChatRoomDTO.toEntity(chatRoomDTO);
-
-        // 데이터베이스에 저장
-        ChatRoom savedChatRoom = chatRoomRepository.save(chatRoom);
-
-        
-        // 선택된 userId로 ChatRoomUsers 엔터티 생성 및 저장
-        for (Long userId : userIds) {
-            User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("Invalid user ID: " + userId));
-            ChatRoomUsers chatRoomUsers = new ChatRoomUsers();
-            chatRoomUsers.setUser(user);
-            chatRoomUsers.setChatroom(savedChatRoom);
-            chatRoomUsersRepository.save(chatRoomUsers);
-        }
-        
-        
-        
-        // Entity -> DTO 변환 후 반환
-        return ChatRoomDTO.toDto(savedChatRoom);
-    }
+//    public ChatRoomDTO createChatRoom(ChatRoomDTO chatRoomDTO, Long[] userIds) {
+//        // DTO -> Entity 변환
+//        ChatRoom chatRoom = ChatRoomDTO.toEntity();
+//
+//        // 데이터베이스에 저장
+//        ChatRoom savedChatRoom = chatRoomRepository.save(chatRoom);
+//
+//        
+//        // 선택된 userId로 ChatRoomUsers 엔터티 생성 및 저장
+//        for (Long userId : userIds) {
+//            User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("Invalid user ID: " + userId));
+//            ChatRoomUsers chatRoomUsers = new ChatRoomUsers();
+//            chatRoomUsers.setUser(user);
+//            chatRoomUsers.setChatroom(savedChatRoom);
+//            chatRoomUsersRepository.save(chatRoomUsers);
+//        }
+//        
+//        
+//        
+//        // Entity -> DTO 변환 후 반환
+//        return ChatRoomDTO.toDto(savedChatRoom);
+//    }
 }
