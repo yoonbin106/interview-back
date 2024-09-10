654d40ee6560a921c4716fb2e168df4bc48d1b30
Elijah <162012881+Elijahuni@users.noreply.github.com>
2024-09-10 22:48:18 +0900

    feat: 음성분석 기능 추가 date: 24.09.10

---
+++ src/main/java/com/ictedu/interview/model/entity/VideoSpeechAnalysis.java
@@ -0,0 +1,48 @@
+package com.ictedu.interview.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "video_speech_analysis")
+public class VideoSpeechAnalysis {
+
+	@Id
+    @SequenceGenerator(name="seq_video_speech_analysis_id", sequenceName = "seq_video_speech_analysis_id", allocationSize = 1, initialValue = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_video_speech_analysis_id")
+    private Long id;
+
+    @Column(name = "video_id")
+    private Long videoId;
+
+    @Lob
+    @Column(columnDefinition = "CLOB")
+    private String transcription;
+
+    @Column(name = "sentiment_overall")
+    private String sentimentOverall;
+
+    @Column(name = "sentiment_confidence")
+    private Double sentimentConfidence;
+
+    @Lob
+    @Column(columnDefinition = "CLOB")
+    private String summary;
+
+    @ColumnDefault("SYSDATE")
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+
+}
